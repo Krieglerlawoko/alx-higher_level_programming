179e9029f075b3bd0694fc5fe84487dcf50ed589
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-13 11:52:18 -0700

    change

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix
+
+    for a in range(len(matrix)):
+        new_matrix[a] = list(map(lambda j: j*j,matrix[a]))
+
+        return (new_matrix)
