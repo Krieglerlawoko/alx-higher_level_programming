0acbf7f4a807a643e332ce2569539b77b3fc5a9c
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-13 11:57:53 -0700

    new change

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix
+    new_matrix = matrix.copy()
 
     for a in range(len(matrix)):
         new_matrix[a] = list(map(lambda j: j*j,matrix[a]))
