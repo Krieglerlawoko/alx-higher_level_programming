076ace572e59af49662eb62c8f672cb9c76db27d
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-13 12:22:02 -0700

    new

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    list2 = list(map(lambda a: replace if a == search else a, my_list))
+    return (list2)
+
---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    list2 = set(my_list)
+    digit = 0
+
+    for a in list2:
+        count += a
+
+    return (digit)
+
