3a2fa79c8fa7995225dfbe7565520867abbf9417
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-14 04:14:59 -0700

    ne

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     matrix2 = matrix.copy()
 
     for a in range(len(matrix)):
-        matrix[a] = list(map(lambda a: a**2, matrix[a]))
+        matrix2[a] = list(map(lambda a: pow(a, 2), matrix[a]))
 
     return (matrix2)
