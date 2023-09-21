52d31f0b1606c2c74422c57b7cbb5d16446fc29f
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-21 08:27:57 -0700

    n

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,4 +6,4 @@ def uppescase(str):
         if ord(charact) <= 122 and ord(charact) >= 97:
             charact = chr(ord(c) - 32)
         print("{}".format(charact), end="")
-        print("")
+    print("")
