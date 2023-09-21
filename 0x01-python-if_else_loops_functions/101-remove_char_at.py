d432292c9f48a27cebd06c89835a195bb7c50b2f
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-21 05:56:38 -0700

    n

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 
 def romove_char_at(str, n):
     """copies a string with out char at n position"""
-    if n is not 0:
+    if n != 0:
         return(str[:n] + str[n+1:])
     else:
         return(str)
