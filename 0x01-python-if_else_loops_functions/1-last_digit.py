b8b72f504c328c5ec8dd7093081f8ba0b9b52f8a
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-21 07:56:21 -0700

    ne

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ num = abs(number) % 10
 if number < 0:
     num = -num
     print("Last digit of {} is {} and is".format(number, num), end="")
-if digit == 0:
+if num == 0:
     print("0")
-elif digit > 5:
+elif num > 5:
     print("greater than 5")
 else:
     print("less than 6 and not 0")
