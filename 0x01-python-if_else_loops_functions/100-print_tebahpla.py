aaee22d344c7f52b992e94047e75baf5f3a2bc4b
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-21 05:53:54 -0700

    new

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,5 @@
 """print alphabet in revers"""
 a = 0
 for chars in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chars(chars - a)), end="")
+    print("{}".format(chr(chars - a)), end="")
     a = 32 if a == 0 else 0
