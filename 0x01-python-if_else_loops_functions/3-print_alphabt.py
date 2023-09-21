96d2e7da33e42b15accfcb8fd084e6945d6c7836
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-21 08:03:34 -0700

    n

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for charcter in range:
+for charcter in range(97, 123):
     if chr(charcter) is not 'e' and chr(charcter) is not 'q':
         print("{}".format(chr(charcter)), end="")
