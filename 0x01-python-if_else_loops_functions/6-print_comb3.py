406426157e1149d0a888713b36f3c902f6fad929
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-21 08:20:48 -0700

    n

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 """prints different combinations of 2 numbers"""
 for num in range(0, 10):
     for num2 in range(num + 1, 10):
-        if num2 == 9 and num 1 == 8:
+        if num2 == 9 and num == 8:
             print("{}{}".format(num, num2))
         else:
             print("{}{}".format(num, num2), end=", ")
