a4db0f89c842fac69a10f756de6fa774c5799f43
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-22 03:47:00 -0700

    n

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -4,7 +4,7 @@ def magic_calculation(a, b, c):
     """matich bytecode"""
 
     if a < b:
-        return(c)
+        return (c)
     if c > b:
-        return(a + b)
-    return(a*b - c)
+        return (a + b)
+    return (a*b - c)
