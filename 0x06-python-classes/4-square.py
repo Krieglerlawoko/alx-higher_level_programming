7d5ed09e7b7a12e1aac2d1cefde9d02cad159d3e
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-26 06:59:43 -0700

    n

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""class Square."""
+
+
+class Square:
+    """square class"""
+
+    def __init__(self, size=0):
+        """Initialize square."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Get or set size of the square."""
+        return (self.__size)
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return current area"""
+        return (self.__size * self.__size)
