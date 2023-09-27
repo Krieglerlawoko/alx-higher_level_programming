280be7202f7363d5c166bcc76d377a1af1d28e35
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-27 08:40:55 -0700

    new

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@
 def no_c(my_string):
     """Remove  characters c and C in string"""
     newlist = [a for a in my_string if a != 'c' and a != 'C']
-    return ("".join(copy))
+    return ("".join(newlist))
