4eff6842cd9bb6f159baa40f78587811a8360af4
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-27 08:02:41 -0700

    new

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_list_integer(my_list=[]):
+    """Print integers in a list."""
+    for a in range(len(my_list)):
+        print("{:d}".format(my_list[a]))
+
---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    """gets an element of a list."""
+    if index > (len(my_list) - 1) or index < 0:
+        return None
+    return (my_list[index])
