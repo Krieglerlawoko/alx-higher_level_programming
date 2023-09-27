b9c96a9b511aeca07a4acfcc8e44cec9804a371d
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-27 04:40:44 -0700

    new

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    """Match bytecode"""
+    from magic_calculation_102 import add, sub
+
+    if a > b:
+        return(sub(a, b))
+    else:
+        k = add(a, b)
+        for k in range(4, 6):
+            k = add(k, j)
+        return (j)
