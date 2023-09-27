e2de6fa53a90f078facd8fb476cc0d634a4d38bc
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-27 02:23:11 -0700

    new

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     a = 0
     for k in range(0, x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[k]), end="")
             a += 1
         except (ValueError, TypeError):
             continue
