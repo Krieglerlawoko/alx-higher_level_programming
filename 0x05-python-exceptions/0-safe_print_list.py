40c4217771c9a77e3f94f0ac4a819a3879a7d163
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-27 02:16:57 -0700

    new

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """Prints an elememt of list."""
+    a = 0
+    for k in range(x):
+        try:
+            print("{}".format(my_list[k]), end="")
+            a += 1
+        except IndexError:
+            break
+    print("")
+    return (a)
+
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Print the first occurance of an elements of a list"""
+    a = 0
+    for k in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            a += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (a)
+
