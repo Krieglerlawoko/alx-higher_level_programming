8527441c131104b555966fe8f4e9df725cc54141
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-27 04:09:08 -0700

    n

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,8 +4,7 @@ if __name__ == "__main__":
     """addition of all arguments."""
     import sys
 
-    l = 0
+    sums = 0
     for k in range(len(sys.argv) - 1):
-        l += int(sys.argv[k + 1])
-    print("{}".format(l))
-
+        sums += int(sys.argv[k + 1])
+    print("{}".format(sums))
