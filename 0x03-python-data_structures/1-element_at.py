70b1f56a476bb0663d66e4511b1e80a6572d0f50
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-09-27 08:07:59 -0700

    the

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@
 
 def element_at(my_list, idx):
     """gets an element of a list."""
-    if index > (len(my_list) - 1) or index < 0:
+    if idx > (len(my_list) - 1) or idx < 0:
         return None
-    return (my_list[index])
+    return (my_list[idx])
