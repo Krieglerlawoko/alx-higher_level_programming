8da943792247b768bd247620efb81989837c1642
Krieglerlawoko <Krieglerlawoko@gmail.com>
2023-10-09 14:43:52 +0000

    new

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class of a base geometry"""
+
+
+class BaseGeometry:
+    """A base geometry."""
+
+    def area(self):
+        """unimplemented"""
+        raise Exception("area() is not implemented")
