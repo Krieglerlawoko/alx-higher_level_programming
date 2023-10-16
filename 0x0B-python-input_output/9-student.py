efec25939a87168851cf5e6bf4962a79019a2128
Krieglerlawoko <Krieglerlawoko@gmail.com>
2023-10-16 16:00:39 +0000

    n

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,9 +7,9 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """a new Student initialized"""
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        self.first_name = first_name
     def to_json(self):
         """a dictionary of Student represented"""
         return self.__dict__
