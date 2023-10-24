7fc663fdad3c46acaed5ca3b0d6fe73d9674a7bb
Krieglerlawoko <Krieglerlawoko@gmail.com>
2023-10-24 08:13:47 +0000

    new

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -11,4 +11,4 @@ def append_after(filename="", search_string="", new_string=""):
             if search_string in k:
                 t += new_string
     with open(filename, "w") as l:
-        l.write(text)
+        l.write(t)
