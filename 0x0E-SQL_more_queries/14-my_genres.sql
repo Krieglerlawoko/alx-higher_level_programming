2743cd329d982f38579f11b3744a0507478d92ae
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2024-01-17 12:17:20 +0000

    ne

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
--- hbtn_0d_tvshows database used to lists all genres of the show Dexter
--- Databse used to lists all rows in a table corresponding to all rows in another
+-- hbtn_0d_tvshows database used to lists genres of the show Dexter
+-- Databse used to lists rows in a table corresponding to all rows in another
 SELECT name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
