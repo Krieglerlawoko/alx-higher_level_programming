abd357a5a457b50dc56c75523efa9cbd7e35f8d1
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2024-01-17 12:15:17 +0000

    new

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
--- Comedy shows in the database hbtn_0d_tvshows listed
--- Rows of a database corresponding to a column value listed
-SELECT title
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy'
-GROUP BY title
-ORDER BY title ASC;
+-- hbtn_0d_tvshows database used to lists all genres of the show Dexter
+-- Databse used to lists all rows in a table corresponding to all rows in another
+SELECT name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+GROUP BY name
+ORDER BY name ASC;RDER BY title ASC;
