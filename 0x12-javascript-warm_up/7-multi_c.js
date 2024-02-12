bb6d6af7c0f6f1489e3645046386bb53e1315754
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2024-02-12 10:12:45 +0000

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) || process.argv[2] === undefined)) {
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Missing number of occurrences');
 } else {
-  const a = Number(process.argv[2]);
-  let k = 0;
-  while (k < a) {
+  const x = Number(process.argv[2]);
+  let a = 0;
+  while (a < x) {
     console.log('C is fun');
-    k++;
+    a++;
   }
 }
