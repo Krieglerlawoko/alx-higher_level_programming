b495e47216326618b1145b2bd2e6bae90b3f36c2
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2024-02-13 09:59:04 +0000

    n

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,12 +4,12 @@
 
 void print_python_list_info(PyObject *p)
 {
-	long int s = PyList_Size(p);
+	long int syz = PyList_Size(p);
 	int a;
 	PyListObject *obj = (PyListObject *)p;
 
-	printf("[*] Size of the Python List = %li\n", s);
+	printf("[*] Size of the Python List = %li\n", syz);
 	printf("[*] Allocated = %li\n", obj->allocated);
-	for (a = 0; a < size; a++)
-		printf("Element %a: %s\n", a, Py_TYPE(obj->ob_item[a])->tp_name);
+	for (a = 0; a < syz; a++)
+		printf("Element %i: %s\n", a, Py_TYPE(obj->ob_item[a])->tp_name);
 }
--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -2,20 +2,20 @@ import ctypes
 
 lib = ctypes.CDLL('./libPyList.so')
 lib.print_python_list_info.argtypes = [ctypes.py_object]
-a = ['hello', 'World']
-lib.print_python_list_info(a)
-del a[1]
-lib.print_python_list_info(a)
-a = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
-lib.print_python_list_info(a)
-a = []
-lib.print_python_list_info(a)
-a.append(0)
-lib.print_python_list_info(a)
-a.append(1)
-a.append(2)
-a.append(3)
-a.append(4)
-lib.print_python_list_info(a)
-a.pop()
-lib.print_python_list_info(a)
+l = ['hello', 'World']
+lib.print_python_list_info(l)
+del l[1]
+lib.print_python_list_info(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
+lib.print_python_list_info(l)
+l = []
+lib.print_python_list_info(l)
+l.append(0)
+lib.print_python_list_info(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list_info(l)
+l.pop()
+lib.print_python_list_info(l)
