63ba6d7c0021e325491d7ebf1f6a6774ff273762
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2024-02-13 10:11:57 +0000

    n

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
-cat > #!/usr/bin/python3
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     nw_tup = ()
     tup_1 = tuple_a + (0, 0)
     tup_2 = tuple_b + (0, 0)
-    nw_tup = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    nw_tup = tup_1[0] + tup_2[0], tup_1[1] + tup_2[1]
     return nw_tup
